22a74896b74c2e28659e920cc740da84cc3dca6d
Markus <abramianmedina@gmail.com>
2025-02-10 11:26:13 +0000

    SOLID-Typescript

--- 5.D/index.ts
+++ 5.D/index.ts
@@ -10,6 +10,7 @@ const pushNotifier = new PushNotification();
 const emailService = new NotificationService(emailNotifier);
 const textMessageService = new NotificationService(textMessageNotifier);
 const pushService = new NotificationService(pushNotifier);
+
 emailService.notify("Hello, this is an email notification!");
 textMessageService.notify("Hello, this is a text message notification!");
 pushService.notify("Hello, this is a push notification!");
